08df2e2e5c7de400cbae098644c1735521dc2888
Nicole Corrêa Colasso <nicolecorreacolasso@gmail.com>
2026-06-01 11:48:50 -0300

    footer

---
+++ src/Brecharme/resources/views/layout/_rodape.blade.php
@@ -0,0 +1,44 @@
+<footer class="brecharme-footer">
+            <div class="footer-container">
+                
+                <div class="footer-column">
+                    <h5>Localização</h5>
+                    <a href="https://maps.app.goo.gl/Qv6UfzH5sycVVMWh9" target="_blank">
+                        Rua Azarias Leite, 10-14<br>Bauru-SP
+                    </a>
+                </div>
+
+                <div class="footer-column">
+                    <h5>Horário</h5>
+                    <p>Segunda a sexta-feira, das 9h às 17h<br>Sábados, das 9h às 12h</p>
+                </div>
+
+                <div class="footer-column">
+                    <h5>Siga-nos</h5>
+                    <a href="https://www.instagram.com/brecharme_caritasbauru/" target="_blank">
+                        @brecharme_caritasbauru
+                    </a>
+                </div>
+
+                <div class="footer-column">
+                    <h5>Contato</h5>
+                    <a href="https://api.whatsapp.com/send/?phone=5514991083780&text=Ol%C3%A1+C%C3%A1ritas+Bauru.+Gostaria+de+informa%C3%A7%C3%B5es&type=phone_number&app_absent=0" target="_blank">
+                        (14) 99108-3780
+                    </a>
+                </div>
+
+            </div>
+        </footer>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        
+        <script src="{{ asset('js/cabecalho.js') }}"></script>
+
+        <script>
+            $(document).ready(function(){
+                // Mantém a sua estrutura jQuery viva para futuras interações de formulários
+                console.log("Brecharme pronto!");
+            });
+        </script>
+    </body>
+</html>
