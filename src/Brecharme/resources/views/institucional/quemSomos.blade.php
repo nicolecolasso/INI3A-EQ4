274b4ba5ec453ee0d895c0368bc8c0b51af024eb
nicolecolasso <nicole.colasso@unesp.br>
2026-06-06 17:07:04 -0300

    mudança testo quem somos

--- src/Brecharme/resources/views/institucional/quemSomos.blade.php
+++ src/Brecharme/resources/views/institucional/quemSomos.blade.php
@@ -10,12 +10,10 @@
         <h1 class="quem-somos-titulo">Quem somos?</h1>
         <div class="quem-somos-card">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis 
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                &nbspO Brecharme é um brechó solidário vinculado à Cáritas de Bauru, organização que atua em ações de assistência e desenvolvimento social voltadas à comunidade. <br>
+                &nbspComo uma extensão da Cáritas, o Brecharme tem como objetivo transformar doações de roupas, calçados e acessórios em oportunidades de apoio social, promovendo a sustentabilidade e o consumo consciente.<br>
+                &nbspPor meio da comercialização de peças em bom estado por preços simbólicos, o Brecharme contribui para a manutenção de projetos sociais e amplia o acesso da população a produtos de qualidade e baixo custo. <br>
+                &nbspDessa forma, une solidariedade, responsabilidade social e cuidado com o meio ambiente em benefício da comunidade.
             </p>
         </div>
     </section>
@@ -24,8 +22,7 @@
         <h2 class="quem-somos-subtitulo">Missão</h2>
         <div class="quem-somos-card">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                &nbspPromover a solidariedade e a sustentabilidade por meio da comercialização de roupas e acessórios doados a preços simbólicos, tornando a moda acessível e contribuindo para o bem-estar da comunidade.
             </p>
         </div>
     </section>
@@ -34,8 +31,7 @@
         <h2 class="quem-somos-subtitulo">Visão</h2>
         <div class="quem-somos-card">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                &nbspSer referência em brechó solidário, reconhecido pelo compromisso com a inclusão social, a sustentabilidade e a transformação positiva da comunidade por meio de ações que unem solidariedade e consumo consciente.
             </p>
         </div>
     </section>
@@ -44,8 +40,12 @@
         <h2 class="quem-somos-subtitulo">Valores</h2>
         <div class="quem-somos-card">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                <li> Solidariedade: ajudar pessoas por meio de ações que promovam dignidade e inclusão. </li>
+                <li> Sustentabilidade: incentivar a reutilização de produtos e a redução do desperdício. </li>
+                <li> Respeito: tratar todas as pessoas com igualdade, empatia e acolhimento. </li>
+                <li> Transparência: atuar com ética, responsabilidade e honestidade. </li>
+                <li> Compromisso Social: gerar impacto positivo e contribuir para o desenvolvimento da comunidade. </li>
+                <li> Acessibilidade: oferecer produtos de qualidade por preços justos e acessíveis. </li>
             </p>
         </div>
     </section>
